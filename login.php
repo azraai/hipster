fbe9f1442050729c6c7bf00aa16b69dd33682553
crynobone <crynobone@gmail.com>
2012-05-19 19:02:16 +0800

    Add login page
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ login.php
@@ -0,0 +1,14 @@
+<form id="register">
+	<div class="control-group">
+		<label for="username" class="control-label">Useranme</label>
+		<div class="controls">
+			<input type="text" name="username" id="username">
+		</div>
+	</div>
+	<div class="control-group">
+		<label for="password" class="control-label">Password</label>
+		<div class="controls">
+			<input type="password" name="password" id="password">
+		</div>
+	</div>
+</form>
